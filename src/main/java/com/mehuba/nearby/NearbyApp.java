beb9e2e8ae0688b693724d4355e3d872b2ab962f
zhaoyibin <zhaoyibin@mobike.com>
2018-02-12 23:37:52 +0800

    test ApplicationListener & CommandLineRunner

--- src/main/java/com/mehuba/nearby/NearbyApp.java
+++ src/main/java/com/mehuba/nearby/NearbyApp.java
@@ -1,12 +1,33 @@
 package com.mehuba.nearby;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
+import java.util.Arrays;
+
 /**
  * Created by zhaoyibin on 18:11 12/02/2018
  */
 
-
+@SpringBootApplication
 public class NearbyApp {
     public static void main(String[] args) {
+        SpringApplication.run(NearbyApp.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Inspect Spring Boot created beans.");
+            String[] names = ctx.getBeanDefinitionNames();
+            Arrays.sort(names);
+            for (String name : names) {
+                System.out.println("-" + name);
+            }
+        };
     }
 }
---
+++ src/main/java/com/mehuba/nearby/TestApplicationListener.java
@@ -0,0 +1,20 @@
+package com.mehuba.nearby;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhaoyibin on 23:14 2018/2/12
+ */
+@Component
+public class TestApplicationListener implements ApplicationListener{
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        if (applicationEvent instanceof ContextRefreshedEvent) {
+            System.out.println("ContextRefreshedEvent");
+        }
+        System.out.println(applicationEvent);
+    }
+}
